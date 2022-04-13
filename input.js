f0990b15f527447c2dd62add72fa56ffb0d1b4e1
JRam2022 <jasonram778@gmail.com>
2022-04-13 16:35:32 -0700

    Created to handle user input

---
+++ input.js
@@ -0,0 +1,21 @@
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  process.stdout.write('.');
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput()
+
+module.exports = { setupInput };
