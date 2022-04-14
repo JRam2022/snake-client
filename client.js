6f9f53d4aae5d526b9aa4d301b2af0b2f598334a
JRam2022 <jasonram778@gmail.com>
2022-04-13 18:31:48 -0700

    Completed

--- client.js
+++ client.js
@@ -10,12 +10,9 @@ const connect = function (data) {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log(data);
-  });
-
-  conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write('Name:JJR');
+    console.log(data);
     
 
     //setInterval(() => {
