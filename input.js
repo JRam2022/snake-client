b521761701a182f49e1adddf177148766baa9818
JRam2022 <jasonram778@gmail.com>
2022-04-13 18:31:23 -0700

    Completed

--- input.js
+++ input.js
@@ -1,6 +1,36 @@
 
+//const { connect } = require('./client.js');
 
-const setupInput = function () {
+let connection;
+
+
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  //up
+  if (key === '\u001b[A') {
+    connection.write("Move: up")
+  }
+  //down
+  if (key === '\u001b[B') {
+    connection.write("Move: down")
+  }
+  //right
+  if (key === '\u001b[C') {
+    connection.write("Move: right")
+  }
+  //left
+  if (key === '\u001b[D') {
+    connection.write("Move: left")
+  }
+
+
+};
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,13 +39,7 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (data) {
-  process.stdout.write('.');
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
-setupInput()
+
 
 module.exports = { setupInput };
